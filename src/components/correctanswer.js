51be5867cda13709d075ceb9c50143f18c9b9a34
Rajni Dua <rajni.dua14@gmail.com>
2021-02-19 11:56:47 +1030

    Source Code for Addition

---
+++ src/components/correctanswer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class CorrectAnswer extends Component {
+  state = {};
+  render() {
+    return <p>Congratulations!! Your Answer is Correct.</p>;
+  }
+}
+
+export default CorrectAnswer;
